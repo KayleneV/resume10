5845fd5d0f8a7bbb7e92b3b37c93206b98201f13
Kaylene Van Wyk <95338328+KayleneV@users.noreply.github.com>
2022-03-20 20:30:40 +0200

    added some stuff

--- main.js
+++ main.js
@@ -7,16 +7,18 @@
         if ($(this).scrollTop() > 200){
             $('.navbar').fadeIn('slow').css('display','flex');
         }else{
-            $(.)
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        }else{
+            $('.navbar').fadeOut('slow').css('display', 'none');
         }
     }
 
 
     //Typed Initiate
-    if ($('.typed-text-output').length==1){
+    if ($('.typed-text-output').length ==1){
         var typed_strings = $('.typed-text').text();
-        var typed = new ('.typed-text-output',{
-            strings:typed_strings.split(','),
+        var typed = new Typed('.typed-text-output',{
+            strings:typed_strings.split(', '),
             typeSpeed:100,
             backSpeed:20,
             smartBackspace:false,
