0a697b61b0d4c0ce0c324cf83e6bd30838e4a431
Kaylene Van Wyk <95338328+KayleneV@users.noreply.github.com>
2022-03-23 17:56:55 +0200

    deleted portfolio

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
     if ($('.typed-text-output').length ==1){
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output',{
-            strings:typed_strings.split(', '),
+            strings:typed_strings.split(', ');
             typeSpeed:100,
             backSpeed:20,
             smartBackspace:false,
@@ -32,6 +32,9 @@ $('.skill').waypoint(function (){
         $(this).css("width", $(this).attr("aria-valuenow") + "%");
     });
 },({offset: '80%'});
+
+
+
 
 
 
